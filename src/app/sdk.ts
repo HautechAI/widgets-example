f927351534b7b6542cd813b8b77300e88c5fd86e
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2024-11-12 11:06:31 +0400

    fix: external id

--- src/app/sdk.ts
+++ src/app/sdk.ts
@@ -2,9 +2,6 @@
 
 import { createServerSDK } from "@hautechai/server";
 
-console.log("----");
-console.log(process.env);
-
 const serverSDK = createServerSDK({
   appId: process.env.APP_ID!,
   appKeyId: process.env.APP_KEY_ID!,
@@ -17,7 +14,10 @@ const serverSDK = createServerSDK({
 export const getClientToken = async (): Promise<string> => {
   const userId = "example";
 
-  let account = await serverSDK.root.accounts.getByExternalId({ id: userId });
+  let account = await serverSDK.root.accounts.get({
+    id: userId,
+    externalId: userId,
+  });
   if (!account) {
     account = await serverSDK.root.accounts.create({ externalId: userId });
   }
